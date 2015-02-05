b26fa0e36a92f2f8b7419210e681931d4c076868
Kailey Izard <Kailey.izard@gmail.com>
2015-02-05 10:27:23 -0500

    Updated config.rb

--- config.rb
+++ config.rb
@@ -1,12 +1,17 @@
 ###
 # Compass
 ###
+require "font-awesome-sass"
 
+# Asset paths
+set :css_dir, "stylesheets"
+set :js_dir, "javascripts"
+set :images_dir, "images"
 # Change Compass configuration
 # compass_config do |config|
 #   config.output_style = :compact
 # end
-
+set :url_root, URL_ROOT
 ###
 # Page options, layouts, aliases and proxies
 ###
@@ -55,6 +60,66 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :imageoptim do |options|
+    # print out skipped images
+    options.verbose = false
+
+    # Setting these to true or nil will let options determine them (recommended)
+    options.nice = true
+    options.threads = true
+
+    # Image extensions to attempt to compress
+    options.image_extensions = %w(.png .jpg .gif .svg)
+
+    # # compressor worker options, individual optimisers can be disabled by passing
+    # # false instead of a hash
+    # # options.pngcrush_options  = {:chunks => ['alla'], :fix => false, :brute => false}
+    # # options.pngout_options    = false #{:copy_chunks => false, :strategy => 0}
+    # options.optipng_options  = {:level => 6, :interlace => false}
+    # options.advpng_options    = {:level => 4}
+    # options.jpegoptim_options = {:strip => ['all'], :max_quality => 100}
+    # options.jpegtran_options  = {:copy_chunks => false, :progressive => true, :jpegrescan => true}
+    # options.gifsicle_options  = {:interlace => false}
+    #options.svgo_options      = false
+  end
+end
+# Add bower's directory to sprockets asset path
+after_configuration do
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  sprockets.append_path File.join "#{root}", @bower_config["directory"]
+end
+
+compass_config do |config|
+  # Require any additional compass plugins here.
+  config.add_import_path "bower_components/foundation/scss"
+  
+  # Set this to the root of your project when deployed:
+  config.http_path = "/"
+  config.css_dir = "stylesheets"
+  config.sass_dir = "stylesheets"
+  config.images_dir = "images"
+  config.javascripts_dir = "javascripts"
+
+  # You can select your preferred output style here (can be overridden via the command line):
+  # output_style = :expanded or :nested or :compact or :compressed
+
+  # To enable relative paths to assets via compass helper functions. Uncomment:
+  # relative_assets = true
+
+  # To disable debugging comments that display the original location of your selectors. Uncomment:
+  # line_comments = false
+
+
+  # If you prefer the indented syntax, you might want to regenerate this
+  # project again passing --syntax sass, or you can uncomment this:
+  # preferred_syntax = :sass
+  # and then run:
+  # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+end
+
   # For example, change the Compass output style for deployment
   # activate :minify_css
 
